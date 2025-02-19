ff00d592adc21173f79acf457b1533c7cb0cff9c
Abse <abner.fonseca.melgarejo@gmail.com>
2025-02-19 17:50:31 -0500

    Cambios al reporte y conexiones de base de datos, y al reclamos.html

--- public/js/firebase-config.js
+++ public/js/firebase-config.js
@@ -2,12 +2,12 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebas
 import { getFirestore, collection, addDoc, serverTimestamp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "TU_API_KEY",
-  authDomain: "TU_AUTH_DOMAIN",
-  projectId: "TU_PROJECT_ID",
-  storageBucket: "TU_STORAGE_BUCKET",
-  messagingSenderId: "TU_MESSAGING_SENDER_ID",
-  appId: "TU_APP_ID"
+  apiKey: "AIzaSyAFFldEP4nCA4XU3AvJ6FcDy6mY0eBnke0",
+  authDomain: "kuantum-server.firebaseapp.com",
+  projectId: "kuantum-server",
+  storageBucket: "kuantum-server.firebasestorage.app",
+  messagingSenderId: "13997661825",
+  appId: "1:13997661825:web:64cc3e11b8c2ccf7707780"
 };
 
 const app = initializeApp(firebaseConfig);
